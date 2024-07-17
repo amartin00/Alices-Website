24ec309fddb96bc44753b09df87703e44d0073b5
amartin00 <alicemartin727@gmail.com>
2024-07-17 17:47:34 -0500

    modified:   app.css 	modified:   app.js 	modified:   index.html

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ window.addEventListener('scroll', () => {
     }
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
-    const topnavLinks = document.querySelector('.topnav a');
-  
-    hamburgerMenu.addEventListener('click', function() {
-      topnavLinks.classList.toggle('show');
-    });
-  });
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
